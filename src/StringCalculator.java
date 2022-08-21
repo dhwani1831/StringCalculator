b8163db2528d78b040e23b5dec53e183aab8fefa
Dhwani <dhwani1831@gmail.com>
2022-08-21 22:06:45 +0530

    Passed test numberBiggerThenThousand

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -43,6 +43,10 @@ public class StringCalculator
                 {
                     negative.add(current);
                 }
+                else if(stringToInteger(current)>1000)
+                {
+                    continue;
+                }
                 else
                     sum += stringToInteger(current);
             }

31b780a4928c0c4b330dadbaba99df24e338174e
Dhwani <dhwani1831@gmail.com>
2022-08-21 22:50:19 +0530

    Failed Test case of oddEvenAddition

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -91,6 +91,12 @@ public class StringCalculatorTest
     {
         assertEquals(calculator.add("//1;\n2,3"), 6);
     }
+
+    @Test
+    public void oddEvenAddition() throws Exception
+    {
+        assertEquals(calculator.add("0//[***]\n1***2***3"), 4);
+    }
     
 
 }
